613d733e2cea30dfbabb29134f2a141d0b4d24bc
jptbrandao@gmail.com <brandao@Joaos-MacBook-Pro.local>
2022-07-10 22:16:50 -0500

    feat: parses one single file, prints out players to standard output

--- src/parseHtml.py
+++ src/parseHtml.py
@@ -1,44 +1,58 @@
 from bs4 import BeautifulSoup
+import re
 
-def confirmProperDiv(div):
-    if len(div['class']) > 1:
-        return False
-    if div['class'][0] != 'body':
-        return False
-    return True
+# Player has structure:
+# - name: string
+# - timestap: date (string)
+# - messageId: string
 
-def parseDiv(div):
-    return
+def getTimeStamp(div):
+    divTimestamp = div.find('div', class_='pull_right date details')
+    timestamp = re.search("title=\".*\"", str(divTimestamp)).group().split('"')[1]
+    return timestamp
+
+def getPlayerName(msgId, soup):
+    for i in range(0, 20):
+        nextNumber = int(msgId.split('message')[1])-i
+        currentMsgId = 'message'+ str(nextNumber)
+        msgBlock = soup.find('div', id=currentMsgId)
+        nameDiv = msgBlock.find('div', class_='from_name')
+        if nameDiv != None:
+            return nameDiv.text.strip()
+
+def extractPlayerFromBlockMessage(msgId, soup):
+    msgBlock = soup.find('div', id=msgId)
+    timestamp = getTimeStamp(msgBlock)
+    playerName = getPlayerName(msgId, soup)
+    player = { "name": playerName, "date": timestamp, "messageId": msgId }
+    return player
+
+
+def confirmMessageBlock(msgBlock, hashtag):
+    regexMatch = re.search(hashtag, str(msgBlock))
+    if regexMatch == None:
+        return False
+    return msgBlock.find('img') != None
+
+def getValidPlayersBidFromFile(filename):
+    f = open(filename, "r")
+    index = f.read()
+    soup = BeautifulSoup(index, 'html.parser')
+    msgIdList = list(map(lambda x: x.get('id') , soup.find_all('div', id=True)))
+    confirmedList = []
+    for msgId in msgIdList:
+        msgBlock = soup.find('div', id=msgId)
+        if confirmMessageBlock(msgBlock, "#maromba23"):
+            confirmedList.append(msgId)
+
+    playerList = []
+    for msgId in confirmedList:
+        player = extractPlayerFromBlockMessage(msgId, soup)
+        playerList.append(player)
+    print(playerList)
+
 
 
 filename = "messages.html"
-f = open(filename, "r")
-index = f.read()
+getValidPlayersBidFromFile(filename)
 
-print(index)
-print()
-print()
-
-soup = BeautifulSoup(index, 'html.parser')
-divs = soup.find_all('div', class_="body")
-
-divs = list(filter(confirmProperDiv, divs))
-
-print(len(divs))
-print(divs[0])
-print()
-name = divs[0].find('div', class_='from_name')
-print(name.contents[0].strip())
-message = divs[0].find('div', class_='text')
-print(message.contents[0].strip())
-exit()
-
-for idx in range(0, 10):
-    print('div:', divs[idx])
-    print('div-class:', divs[idx]['class'])
-    print('div-contents:', divs[idx].contents)
-    print('is class im lookingfor:', confirmProperDiv(divs[idx]))
-    print('\n\n\n')
-
-
-#hello()
